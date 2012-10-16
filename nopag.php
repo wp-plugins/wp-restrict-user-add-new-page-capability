9e312a40c582c2a397314e63ccc346009bcc963f
RSPublishing <RSPublishing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-16 07:52:27 +0000

    First commit of wp-restrict-user-add-new-page-capability
    git-svn-id: https://plugins.svn.wordpress.org/wp-restrict-user-add-new-page-capability/trunk@613095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nopag.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+  Plugin Name: WP Restrict User 'ADD NEW PAGE' Capability
+  Plugin URI: http://wordpress.org/extend/plugins/wp-restrict-user-add-new-page-capability/
+  Description: This plugin will disable all non-admin users from creating new pages in your blog. The "add new - Page" submenu item will also be hidden in the users dashboard.
+  Version: 1.0
+  Author: RS Publishing
+  License: GPLv2
+ */
+
+/*
+  Copyright 2012  Rynaldo Stoltz  (email : rcstoltz@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+add_action('admin_init','capb_mod');
+add_action('admin_menu','men_mod');
+add_action('admin_head','hide_anbu');
+add_action('admin_menu','adm_redir');
+add_action('admin_init','perm_notice');
+
+function capb_mod() {
+  $editor_role = get_role('editor');
+  $editor_role -> remove_cap('publish_pages');
+  $author_role = get_role('author');
+  $author_role -> remove_cap('publish pages');
+}
+
+function men_mod() {
+  global $submenu;
+  unset($submenu['edit.php?post_type=page'][10]);
+  $submenu['edit.php?post_type=page'][10][1] = 'publish_pages';
+}
+
+function hide_anbu() {
+  global $current_screen;
+  if($current_screen->id == 'edit-page' && !current_user_can('publish_pages'))
+  {
+    echo '<style>.add-new-h2{display: none;}</style>';
+  	}
+  }
+
+function adm_redir() {
+  $result = stripos($_SERVER['REQUEST_URI'], 'post-new.php?post_type=page');
+  if ($result!==false && !current_user_can('publish_pages')) {
+    wp_redirect(get_option('siteurl') . '/wp-admin/index.php?permissions_error=true');
+ 	}
+}
+
+function dbo_noti() {
+  	echo "<div id='permissions-warning' class='error fade'><p><strong>".__('You do not have permission to access that page.')."</strong></p></div>";
+}
+
+function perm_notice() {
+  if($_GET['permissions_error'])
+  {
+    add_action('admin_notices', 'dbo_noti');
+  }
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== WP Restrict User 'ADD NEW PAGE' Capability ===
+
+Contributors: RS Publishing
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FW2K3299SKTR4
+Tags: restrict, non-admin, user, capability, role, prevent, limits, new-page, remove, stop, hide, disable, add new, page
+Requires at least: 2.7
+Tested up to: 3.4.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin will disable all non-admin users (with capabilities), from creating and adding new pages in your blog.
+
+
+== Description ==
+
+This plugin will disable all non-admin users (with capabilities), from creating and adding new pages in your blog.
+Works very well with the 'WP Restrict User 'ADD NEW POST' Capability' plugin (to prevent non-admins from creating and adding new POSTS), to your blog. 
+The "add new page" submenu will also be hidden from the non-admin user.
+
+
+== User Posting Capabilities with this plugin ==
+
+This plugin specifically prevents and disables the 'EDITOR' as well as the 'AUTHOR' level users from creating and adding new PAGES from the user dashboard (and within your blog). All 
+other capabilities of the specified user levels, however, will remain the same.
+
+
+== Installation ==
+
+1. Download the nopage.zip file
+2. Upload and extract the contents of nopage.zip to your wp-content/plugins/folder
+3. Activate the WP Restrict User 'ADD NEW PAGE' Capability in your WP-admin
+4. Done!
+
+== Frequently Asked Questions ==
+
+None
+
+
+== Feedback, Questions, Help, and Suggestions ==
+
+Just post a mail to: rcstoltz@gmail.com with email subject: WP Restrict User 'ADD NEW PAGE' Capability
+
+
+== Upgrade Notice ==
+
+None
+
+== Screenshots ==
+
+1.Before activating the plugin
+2.After activating the plugin
+
+== Changelog ==
+
+None
+
+= Version 1.0 =
+
+First Release Version
+
+== Thumbs Up ==
+
+Like our WP antiProxy plugin? Don't forget to give us a rating.
